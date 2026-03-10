5afd14f634cfcfe8366da4c0727c8c5e9f81c61a
soph <sevanisko04@gmail.com>
2026-03-09 23:22:49 -0400

    Adding diploma download and some assets

--- src/pages/Qualifications.jsx
+++ src/pages/Qualifications.jsx
@@ -1,4 +1,29 @@
+import { useEffect } from 'react';
+import { usePopup } from '../context/PopupContext';
+import DiplomaPdf from '../assets/Diploma.pdf';
+
 export default function Qualifications() {
+  const { createPopup } = usePopup();
+
+  useEffect(() => {
+    createPopup({
+      title: 'My Education',
+      children: (
+        <div style={{fontFamily: 'Courier New', textAlign: 'center', display: 'flex', flexDirection: 'column', height: '100%', minHeight: 0, boxSizing: 'border-box' }}>
+          I have a Bachelors of Science in Computer Science and Applied Mathematics, and minors in Cybersecurity and Statistics.
+          <div className="popup-media">
+            <a href={DiplomaPdf} download="EvaniskoSophia_Diploma.pdf"><strong>Download My Diploma</strong></a>
+          </div>
+        </div>
+      ),
+      widthPercent: 12,
+      heightPercent: 20,
+      xPercent: 15,
+      yPercent: 2,
+    });
+
+  }, [createPopup]);
+
   return (
     <div className="qualifications-page">
       <section className="qualifications">
